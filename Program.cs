b9712298c854f6501feb3052ef85ba07c5c7e56b
Blentin05 <111045987+Blentin05@users.noreply.github.com>
2022-11-10 11:13:48 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Prk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> woerter = new List<string>();
+            woerter.Add("cat"); 
+            woerter.Add("dog"); 
+            woerter.Add("one");
+            woerter.Add("two");
+            woerter.Add("three");
+            woerter.Add("four");
+            woerter.Add("five");
+            woerter.Add("six");
+            woerter.Add("seven"); 
+           
+            foreach (string name in woerter)
+            {
+                Convert.ToString(woerter);
+                Console.WriteLine(name);
+                Console.ReadLine();         
+            }
+
+            if (woerter[0] == "Katze")
+            {
+
+                Console.WriteLine("Richtig");
+            }
+            else
+            {
+                Console.WriteLine("falsch");
+            }
+           
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}      
